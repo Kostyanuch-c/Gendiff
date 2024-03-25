296e591785d7daf71244c57a12a54acf95fd8b7e
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-03-25 13:33:02 +0500

    Update gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3.10
+import argparse
+
 
 
 def main():
-    print("Welcome to the Gendiff!")
+    parser = argparse.ArgumentParser(description='File comparison')
+    parser.add_argument('first_file', type=str, help='Input first file')
+    parser.add_argument('second_file', type=str, help='Input second file')
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
