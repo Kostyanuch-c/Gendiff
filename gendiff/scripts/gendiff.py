226d03d8ecd6b6e66825faf6be46e056f8d74053
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-03-25 14:13:41 +0500

    Update sctucture

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,11 +2,16 @@
 import argparse
 
 
-
 def main():
-    parser = argparse.ArgumentParser(description='File comparison')
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str, help='Input first file')
     parser.add_argument('second_file', type=str, help='Input second file')
+    parser.add_argument(
+        '-f', '--format',
+        type=str,
+        default='json',
+        help='set format of output'
+    )
     args = parser.parse_args()
 
 
