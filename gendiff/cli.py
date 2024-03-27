f7bad351aafc8bff44197d98a848d02b296cf3eb
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-03-27 13:01:18 +0500

    Update files

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -5,8 +5,12 @@ def parser_args():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('first_file', type=str, help='Input path to first file')
-    parser.add_argument('second_file', type=str, help='Input path to second file')
+    parser.add_argument('first_file',
+                        type=str,
+                        help='Input path to first file')
+    parser.add_argument('second_file',
+                        type=str,
+                        help='Input path to second file')
     parser.add_argument(
         '-f', '--format',
         type=str,
