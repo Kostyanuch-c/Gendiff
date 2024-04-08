a5ac7ad29e2fa77b722bd63b3b497ead867be497
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-04-08 17:22:24 +0500

    Fix

--- tests/test_pars_files.py
+++ tests/test_pars_files.py
@@ -1,6 +1,6 @@
 import pytest
 import gendiff.pars_files
-git 
+
 
 @pytest.mark.parametrize("test_input,expected", [(None, 'null'), (True, 'true'), (123, '123')])
 def test_get_json_value(test_input, expected):
