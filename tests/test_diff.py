f413ef8a08dbdcdb4df114c25384048f2103ff0c
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-04-14 16:29:55 +0500

    Fix problems

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -2,7 +2,7 @@ import pytest
 import gendiff
 
 FORMATS = ('stylish', 'plain', 'json')
-PATH_FILES = ('tests/fixtures/volume_result.txt', 'tests/fixtures/flat_result.txt', 'tests/fixtures/json_result.txt')
+PATH_RESULT = ('tests/fixtures/volume_result.txt', 'tests/fixtures/flat_result.txt', 'tests/fixtures/json_result.txt')
 
 
 @pytest.fixture()
@@ -29,11 +29,11 @@ def test_with_wrong_formatters(input_json, formats):
 
 @pytest.mark.parametrize("formats", FORMATS)
 def test_with_wrong_type_files(formats):
-    result = gendiff.generate_diff(PATH_FILES[0], PATH_FILES[1], formats)
+    result = gendiff.generate_diff(PATH_RESULT[0], PATH_RESULT[1], formats)
     assert result == 'Not accepted file type!'
 
 
-@pytest.mark.parametrize("formats, path", zip(FORMATS, PATH_FILES))
+@pytest.mark.parametrize("formats, path", zip(FORMATS, PATH_RESULT))
 def test_normal_json_file_diff(input_json, formats, path):
     test_path1, test_path2 = input_json.split()
     result = gendiff.generate_diff(test_path1, test_path2, formats)
@@ -41,15 +41,15 @@ def test_normal_json_file_diff(input_json, formats, path):
         assert result == file.read()
 
 
-@pytest.mark.parametrize("formats, path", zip(FORMATS, PATH_FILES))
-def test_normal_yaml_file_diff(input_yaml, formats, path):
+@pytest.mark.parametrize("formats, path", zip(FORMATS, PATH_RESULT))
+def test_normal_different_type_diff(input_yaml, formats, path):
     test_path1, test_path2 = input_yaml.split()
     result = gendiff.generate_diff(test_path1, test_path2, formats)
     with open(path, 'r') as file:
         assert result == file.read()
 
 
-@pytest.mark.parametrize("formats, path", zip(FORMATS, PATH_FILES))
+@pytest.mark.parametrize("formats, path", zip(FORMATS, PATH_RESULT))
 def test_normal_yaml_file_diff(input_yaml_and_json, formats, path):
     test_path1, test_path2 = input_yaml_and_json.split()
     result = gendiff.generate_diff(test_path1, test_path2, formats)
