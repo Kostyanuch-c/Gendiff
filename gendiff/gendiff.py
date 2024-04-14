5133c2722daacca602a54cae951fd9993c69e936
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-04-14 18:47:12 +0500

    Check problems

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,8 +6,6 @@ from gendiff.formaters import plain, stylish, json_format
 def build_ast(current_dct1, current_dct2, result={}, acc={}):
     if isinstance(current_dct2, str):
         return current_dct2
-    if isinstance(current_dct1, str):
-        return current_dct1
     keys = current_dct1.keys() | current_dct2.keys()
     acc = copy.deepcopy(result)
     result.clear()
@@ -21,21 +19,15 @@ def build_ast(current_dct1, current_dct2, result={}, acc={}):
                            'type': 'delete',
                            'symbol': '-'}
         elif current_dct1[key] == current_dct2[key]:
-            value = build_ast(current_dct2[key], current_dct2[key],
-                              acc, result) \
-                if isinstance(current_dct1[key], dict) \
-                else current_dct1[key]
-
-            result[key] = {'value': value,
+            result[key] = {'value': build_ast(current_dct2[key],
+                                              current_dct2[key],
+                                              acc, result),
                            'type': 'unchanged',
                            'symbol': ' '}
         else:
-            value = build_ast(current_dct1[key], current_dct2[key],
-                              acc, result) \
-                if isinstance(current_dct2[key], dict) \
-                else current_dct2[key]
-
-            result[key] = {'value': value,
+            result[key] = {'value': build_ast(current_dct1[key],
+                                              current_dct2[key],
+                                              acc, result),
                            'sub_value': current_dct1[key],
                            'type': 'changed',
                            'symbol': ' '}
