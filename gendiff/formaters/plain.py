8b938c9b190768a0323c65bfa7e5ceb69cc1a14c
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-05-04 19:53:08 +0500

    check

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -9,25 +9,23 @@ def check_type(value):
 def make_flat(ast, start_line='Property'):  # noqa
     def walk(cur_val, parents):
         lines = []
-        for key in sorted(cur_val.keys()):
+        for key in cur_val.keys():
             cur_parents = f'{parents}.{key}' if parents else f'{key}'
             val = cur_val[key]
             checked_val = check_type(val['value'])
-            if val['type'] == 'unchanged':
-                continue
-            elif val['type'] == 'added':
+            if val['type'] == 'added':
                 lines.append(f"{start_line} '{cur_parents}'"
                              f" was added with value: {checked_val}")
             elif val['type'] == 'delete':
                 lines.append(f"{start_line} '{cur_parents}' was removed")
-            elif (val['type'] == 'changed'
-                  and not isinstance(val['value'], dict)):
-
+            elif val['type'] == 'unchanged':
+                if not isinstance(val['value'], dict):
+                    continue
+                lines.append(walk(val['value'], cur_parents))
+            else:
                 sub_value = check_type(val['sub_value'])
                 lines.append(f"{start_line} '{cur_parents}' was updated."
                              f" From {sub_value} to {checked_val}")
-            else:
-                lines.append(walk(val['value'], cur_parents))
         return '\n'.join(lines)
 
     return walk(ast, '')
