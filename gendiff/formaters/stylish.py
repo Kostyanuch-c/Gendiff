6838762090580782915f7f351cee8ed41253c88d
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-05-04 19:46:26 +0500

    check

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -2,7 +2,7 @@ from itertools import chain
 
 
 def make_volume(ast_dct, replacer=' ', spaces_cnt=4, lft_shift=2):
-    def walk(current_value, depth):
+    def walk(current_value, depth, flag=None):
         if not isinstance(current_value, dict):
             return current_value
 
@@ -11,21 +11,32 @@ def make_volume(ast_dct, replacer=' ', spaces_cnt=4, lft_shift=2):
         current_indent = replacer * depth
         default = object
         lines = []
-        for key in sorted(current_value.keys()):
-            val = current_value[key]
-            if not isinstance(val, dict) or val.get('type', default) == default:
+        for key in current_value.keys():
+            data = current_value[key]
+            if flag:
+                if isinstance(data, dict):
+                    if flag == '-' and data.get('sub_value', default) != default:
+                        val = data["sub_value"]
+                    elif data.get('value', default) != default:
+                        val = data['value']
+                    else:
+                        val = data
+                else:
+                    val = data
                 lines.append(f'{deep_indent}  {key}: '
-                             f'{walk(val, deep_indent_size)}')
-            elif (val['type'] == 'changed' and not
-                    isinstance(val['value'], dict)):
+                             f'{walk(val, deep_indent_size, flag)}')
 
-                lines.append(f'{deep_indent}- {key}: '
-                             f'{walk(val["sub_value"], deep_indent_size)}')
-                lines.append(f'{deep_indent}+ {key}: {val["value"]}')
+            elif data['type'] == 'added' or data['type'] == 'delete':
+                new_flag = '+' if data['type'] == 'added' else '-'
+                lines.append(f'{deep_indent}{data["symbol"]} {key}: '
+                             f'{walk(data["value"], deep_indent_size, flag=new_flag)}')
+            elif data['type'] == 'unchanged':
+                lines.append(f'{deep_indent}{data["symbol"]} {key}: '
+                             f'{walk(data["value"], deep_indent_size)}')
 
             else:
-                lines.append(f'{deep_indent}{val["symbol"]} {key}: '
-                             f'{walk(val["value"], deep_indent_size)}')
+                lines.append(f'{deep_indent}- {key}: {walk(data["sub_value"], deep_indent_size, flag="-")}')
+                lines.append(f'{deep_indent}+ {key}: {walk(data["value"], deep_indent_size, flag="+")}')
 
         result = chain("{", lines, [current_indent + "}"])
         return '\n'.join(result)
